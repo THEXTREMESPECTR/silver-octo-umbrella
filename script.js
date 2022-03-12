2524ea93aef3d9fafb5e79b1b5c0e319b889dbad
THEXTREMESPECTR <93404684+THEXTREMESPECTR@users.noreply.github.com>
2022-03-12 08:02:11 -0600

    Added login files

---
+++ script.js
@@ -0,0 +1,32 @@
+ function passCheck() {
+   
+            var input = document.getElementById('password').value;
+
+            if (input == 'Darkrai890') {
+               window.location.replace("/not-a-rickroll.html");
+            }
+            else {
+            if(input == 'CresseliaIsDodo'){
+              window.alert('good job you are cultured')
+            }
+              else{
+              var incorrect = 
+                alert('Congratulations You Are Dodo');
+                incorrect.style.display = "Block";
+                return false;
+            }
+        }
+        }  
+//  function auth(event) {
+     //     event.preventDefault();
+
+       //   var email = document.getElementById("email").value;
+         // var password = document.getElementById("password").value;
+
+          //if (email === "admin@gmail.com" && password === "user") {
+            //   window.location.replace("/upload.html");
+          //} else {
+            //  alert("Invalid information");
+              //return;
+          //}
+    //}
